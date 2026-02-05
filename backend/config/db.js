df2df116096b66c2ac7e661ce41f720e7a1046fb
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-02-05 07:26:03 +0530

    password hiding in the db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect('mongodb+srv://sandeepsidar2006_db_user:v6OPeg4q1lwG1294@cluster0.ee2ug9m.mongodb.net/?appName=Cluster0', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("Error:", error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
